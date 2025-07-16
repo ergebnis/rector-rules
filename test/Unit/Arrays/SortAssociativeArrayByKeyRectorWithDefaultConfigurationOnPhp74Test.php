db41a54af83abb2ed21e826dcd8a57663843a79e
Andreas Möller <am@localheinz.com>
2025-07-16 16:16:55 +0200

    Enhancement: Allow configuring SortAssociativeArrayByKeyRector

---
+++ test/Unit/Arrays/SortAssociativeArrayByKeyRectorWithDefaultConfigurationOnPhp74Test.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/rector-rules
+ */
+
+namespace Arrays;
+
+use Rector\Testing;
+
+/**
+ * @covers \Ergebnis\Rector\Rules\Arrays\SortAssociativeArrayByKeyRector
+ *
+ * @requires PHP <= 7.4
+ *
+ * @see https://www.php.net/manual/en/migration80.incompatible.php#migration80.incompatible.core.string-number-comparision
+ */
+final class SortAssociativeArrayByKeyRectorWithDefaultConfigurationOnPhp74Test extends Testing\PHPUnit\AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): iterable
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/../../Fixture/Arrays/SortAssociativeArrayByKeyRector/WithDefaultConfigurationOnPhp74');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/../../Fixture/Arrays/SortAssociativeArrayByKeyRector/WithDefaultConfigurationOnPhp74/config/configured_rule.php';
+    }
+}
---
+++ test/Unit/Arrays/SortAssociativeArrayByKeyRectorWithDirectionDescTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/rector-rules
+ */
+
+namespace Ergebnis\Rector\Rules\Test\Unit\Arrays;
+
+use Rector\Testing;
+
+/**
+ * @covers \Ergebnis\Rector\Rules\Arrays\SortAssociativeArrayByKeyRector
+ */
+final class SortAssociativeArrayByKeyRectorWithDirectionDescTest extends Testing\PHPUnit\AbstractRectorTestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): iterable
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/../../Fixture/Arrays/SortAssociativeArrayByKeyRector/WithDirectionDesc');
+    }
+
+    public function provideConfigFilePath(): string
+    {
+        return __DIR__ . '/../../Fixture/Arrays/SortAssociativeArrayByKeyRector/WithDirectionDesc/config/configured_rule.php';
+    }
+}

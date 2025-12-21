002a73d36d1b092e85b343206afe48008f190b60
Andreas Möller <am@localheinz.com>
2025-12-21 10:22:36 +0100

    Fix: Improve examples

---
+++ test/Fixture/Miscellaneous/Apple.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/rector-rules
+ */
+
+final class Apple
+{
+}
---
+++ test/Fixture/Miscellaneous/Namespaced/Mango.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2023-2025 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/rector-rules
+ */
+
+namespace Namespaced;
+
+final class Mango
+{
+}

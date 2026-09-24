475f0411ebd2a173399216e97c18f7999782749d
Andreas Möller <am@localheinz.com>
2026-09-24 07:27:40 +0200

    Fix: Use PHPUnitSetList::COMPOSER_BASED instead of removed PHPUnitSetList::PHPUNIT_90

--- rector.php
+++ rector.php
@@ -50,6 +50,6 @@ return static function (Config\RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->sets([
-        PHPUnit\Set\PHPUnitSetList::PHPUNIT_90,
+        PHPUnit\Set\PHPUnitSetList::COMPOSER_BASED,
     ]);
 };
